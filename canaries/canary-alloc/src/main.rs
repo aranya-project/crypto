168421c6da19a31f424dd6bc8df45228bd537cb1
Eric Lagergren <elagergren@spideroak.com>
2025-01-21 09:54:27 -0800

    init
    Signed-off-by: Eric Lagergren <elagergren@spideroak.com>

---
+++ canaries/canary-alloc/src/main.rs
@@ -0,0 +1,18 @@
+#![cfg(not(any(test, doctest)))]
+#![no_std]
+#![no_main]
+
+extern crate aranya_base58;
+extern crate aranya_buggy;
+extern crate aranya_crypto;
+extern crate aranya_crypto_core;
+extern crate aranya_trouble;
+
+#[cfg(target_os = "none")] // hack to please rust-analyzer
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[allow(unused)]
+fn main() {}
